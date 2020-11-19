8296ed36fedf03b5186ee13a952fe7ac665e2407
jackalope153 <64624827+jackalope153@users.noreply.github.com>
2020-11-18 21:43:05 -0600

    Add files via upload

--- Project3.sol
+++ Project3.sol
@@ -1,121 +1,77 @@
-// contracts/GameItem.sol
-//SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
-
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
-
-
-
-
-      
-contract Registration is ERC721Full{
-    using Counters for Counters.Counter;
+//contract PropertyCrowdSale is ERC721Full, Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale { }
+contract Registration {
     
     struct Property {
-        uint propertyID;
-        uint userID;
+        uint UserID;
         string URI;
-        
     }
     
     struct User {
-        uint userID;
         address Address;
-        
-    }
-    struct PropertyManagement {
-        uint PropertyManagementID;
-        address _address;
-        
+        string URI;
     }
     
-    Counters.Counter private propertyIDS;
+    struct PropertyManagement {
+        address Address;
+        string URI;
+    }
+    
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private PropertyIDS;
     Counters.Counter private UserIDS;
     Counters.Counter private PropertyManagementIDS;
     
-    
-    //mapping token id to the property
-    //Users (userID => userinfo)
-    //Property (propertyID => propertyinfo)
-    //PropertyManagement (pmuserID => pmuserinfo)
     //Tokens (propertyID => current token balance)
-        
+    mapping(uint => uint[]) tokens;
     mapping(uint => Property) Properties;
     mapping(uint => User) Users;
     mapping(uint => PropertyManagement) PropertyManagers;
     //mapping(uint => mapping(uint => tokenID)) public Properties;
     
-    // Transfer of Property Ownership (amount of tokens, userID, propertyID) 
-   event TransferTokens (uint tokens , uint userID, uint propertyID);
-   
-    // TOken Balance available to purchase (propertyID) 
-    event TokenBalance (uint);
+    // Transfer of Property Ownership (amount of tokens, userID, propertyID)
+    event TransferTokens(uint tokens, uint userID, uint propertyID);
     
+    // Token Balance available to purchase (propertyID)
+    event TokenBalance(uint propertyID);
     
-    constructor() public {   }
+    event RegisterProperty(uint propertyID, uint userID);
     
+    constructor() public { }
     
-    //taking in a userID of owner, property uri, market value of property and total supply of tokens (100) (ether conversion in python), validation id
-    //return propertyID
-    //mint tokens
-    // error message for validation id not beinf correct, or 
-   function registerProperty(address propertyAddress, string memory propertyURI) public returns(uint) {
-        propertyIDS.increment();
-        uint propertyID = propertyIDS.current();
-        // fix this part
-        Properties[propertyID] = Property(1, 2, "");
+    function registerProperty(uint userId, string memory propertyURI) public returns(uint) {
+        PropertyIDS.increment();
+        uint propertyID = PropertyIDS.current();
+        Properties[propertyID] = Property(userId, propertyURI);
+        
+        //
+        
+        emit RegisterProperty(propertyID, userId);
         
         return propertyID;
-   }
-   
-   
-    //input wallet address, IPFS to personal info
-    //return userid
+    }
+    
     function registerUser(address userAddress, string memory userURI) public returns(uint) {
-        //userIDS.increment();
-        //uint userID = userIDS.current();
-        //users[userID] = User(userURI, userAddress);
-        
-        //return userID;
-        return 0;
-    }  
+        UserIDS.increment();
+        uint userID = UserIDS.current();
+        Users[userID] = User(userAddress, userURI);
+        return userID;
+    }
     
-    //input wallet address, IPFS to personal info
-    //output propertymanagementID
-    function registerPropertyManagement(address PropertyManagementAddress) public returns(uint) {
-        //propertymanagementIDS.increment();
-        //uint propertymanagementID = propertymanagementIDS.current();
-        //propertymanagement[propertymanagementID] = PropertyManagement(PropertyManagementAddress);
-        
-        //return propertymanagementID;   
-        return 0;
-        
-    //only for property management, pull token per user token amount/100 * revenue transfer ether to user minus admin fee and maintenance
-    function profit splitter
-    
-    ///validation id check done in python
-    
-    
-    ///new contract with crowdasle function
-         //create token, give owner address, 
-        ///available tokens
-        
-        
-    //propety investment report 
-        //how many properties does user own, what revenue generated?
-        
-    // add minting to registeryproperty function
-    
-    
-    
-    }*/
-    
-    
+    function registerPropertyManagement(address propertyManagementAddress, string memory propertyManagemenURI) public returns(uint) {
+        PropertyManagementIDS.increment();
+        uint propertymanagementID = PropertyManagementIDS.current();
+        PropertyManagers[propertymanagementID] = PropertyManagement(propertyManagementAddress, propertyManagemenURI);
+        return propertymanagementID;
+    }
 }
