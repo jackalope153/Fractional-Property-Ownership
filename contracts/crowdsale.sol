5dd166e7bbaa20937b3693b61d85907231096130
reynoldo slater <niksandnaks@gmail.com>
2020-11-19 22:37:01 -0600

    adding to repo

--- contracts/crowdsale.sol
+++ contracts/crowdsale.sol
@@ -1,63 +1,121 @@
+// contracts/GameItem.sol
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
-import "./propertycoinERC20.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
 
-contract PropertyCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
+
+
+      
+contract Registration is ERC721Full{
+    using Counters for Counters.Counter;
     
-        
-    constructor(
-        uint rate,
-        address payable wallet,
-        PropertyCoin token,
-        uint goal,
-        uint open, 
-        uint close
-    )
-       
-        Crowdsale(rate, wallet, token)
-        CappedCrowdsale(goal)
-        RefundableCrowdsale(goal)
-        TimedCrowdsale(open, close)
-        
-        
-        public
-    {
-        
+    struct Property {
+        uint propertyID;
+        uint userID;
+        string URI;
         
     }
-}
-
-contract PropertySaleDeployer {
-
-    address public token_sale_address;
-    address public token_address;
-
-    constructor(
-        string memory name,
-        string memory symbol,
-        address payable wallet,
-        uint goal
+    
+    struct User {
+        uint userID;
+        address Address;
         
-        
-        
-    )
-        public
-    {
-        
-        PropertyCoin token = new PropertyCoin(name, symbol, 0);
-        token_address = address(token);
-        
-        
-        PropertyCrowdsale property_sale = new PropertyCrowdsale(1, wallet, token, goal, now, now + 4 weeks);
-        token_sale_address = address(property_sale);
-        
-        token.addMinter(token_sale_address);
-        token.renounceMinter();
     }
+    struct PropertyManagement {
+        uint PropertyManagementID;
+        address _address;
+        
+    }
+    
+    Counters.Counter private propertyIDS;
+    Counters.Counter private UserIDS;
+    Counters.Counter private PropertyManagementIDS;
+    
+    
+    //mapping token id to the property
+    //Users (userID => userinfo)
+    //Property (propertyID => propertyinfo)
+    //PropertyManagement (pmuserID => pmuserinfo)
+    //Tokens (propertyID => current token balance)
+        
+    mapping(uint => Property) Properties;
+    mapping(uint => User) Users;
+    mapping(uint => PropertyManagement) PropertyManagers;
+    //mapping(uint => mapping(uint => tokenID)) public Properties;
+    
+    // Transfer of Property Ownership (amount of tokens, userID, propertyID) 
+   event TransferTokens (uint tokens , uint userID, uint propertyID);
+   
+    // TOken Balance available to purchase (propertyID) 
+    event TokenBalance (uint);
+    
+    
+    constructor() public {   }
+    
+    
+    //taking in a userID of owner, property uri, market value of property and total supply of tokens (100) (ether conversion in python), validation id
+    //return propertyID
+    //mint tokens
+    // error message for validation id not beinf correct, or 
+   function registerProperty(address propertyAddress, string memory propertyURI) public returns(uint) {
+        propertyIDS.increment();
+        uint propertyID = propertyIDS.current();
+        // fix this part
+        Properties[propertyID] = Property(1, 2, "");
+        
+        return propertyID;
+   }
+   
+   
+    //input wallet address, IPFS to personal info
+    //return userid
+    function registerUser(address userAddress, string memory userURI) public returns(uint) {
+        //userIDS.increment();
+        //uint userID = userIDS.current();
+        //users[userID] = User(userURI, userAddress);
+        
+        //return userID;
+        return 0;
+    }  
+    
+    //input wallet address, IPFS to personal info
+    //output propertymanagementID
+    function registerPropertyManagement(address PropertyManagementAddress) public returns(uint) {
+        //propertymanagementIDS.increment();
+        //uint propertymanagementID = propertymanagementIDS.current();
+        //propertymanagement[propertymanagementID] = PropertyManagement(PropertyManagementAddress);
+        
+        //return propertymanagementID;   
+        return 0;
+        
+    //only for property management, pull token per user token amount/100 * revenue transfer ether to user minus admin fee and maintenance
+    function profit splitter
+    
+    ///validation id check done in python
+    
+    
+    ///new contract with crowdasle function
+         //create token, give owner address, 
+        ///available tokens
+        
+        
+    //propety investment report 
+        //how many properties does user own, what revenue generated?
+        
+    // add minting to registeryproperty function
+    
+    
+    
+    }*/
+    
+    
 }
